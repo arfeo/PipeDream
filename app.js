9b04f9dfaf0f31bc3e520844e4b359e93f2b9377
Leonid Belikov <leonid@belikov.net>
2018-04-15 21:59:15 +0300

    animation fix

--- app.js
+++ app.js
@@ -18,7 +18,10 @@ const constants = {
 			 */
 			type: 0,
 			outlets: [
-				[ 0 ], [ 1 ], [ 2 ], [ 3 ],
+				[ 0 ],
+				[ 1 ],
+				[ 2 ],
+				[ 3 ],
 			],
 		},
 		{
@@ -31,7 +34,10 @@ const constants = {
 			 */
 			type: 1,
 			outlets: [
-				[ 0, 2 ], [ 1, 3 ], [ 2, 0 ], [ 3, 1 ],
+				[ 0, 2 ],
+				[ 1, 3 ],
+				[ 2, 0 ],
+				[ 3, 1 ],
 			],
 		},
 		{
@@ -44,7 +50,10 @@ const constants = {
 			 */
 			type: 2,
 			outlets: [
-				[ 0 ], [ 1 ], [ 2 ], [ 3 ],
+				[ 0 ],
+				[ 1 ],
+				[ 2 ],
+				[ 3 ],
 			],
 		},
 		{
@@ -57,7 +66,10 @@ const constants = {
 			 */
 			type: 3,
 			outlets: [
-				[ 0, 1, 2, 3 ], [ 1, 2, 3, 0 ], [ 2, 3, 0, 1 ], [ 3, 0, 1, 2 ],
+				[ 0, 1, 2, 3 ],
+				[ 1, 2, 3, 0 ],
+				[ 2, 3, 0, 1 ],
+				[ 3, 0, 1, 2 ],
 			],
 		},
 		{
@@ -70,7 +82,10 @@ const constants = {
 			 */
 			type: 4,
 			outlets: [
-				[ 0, 1, 3 ], [ 0, 1, 2 ], [ 1, 2, 3 ], [ 2, 3, 0 ],
+				[ 0, 1, 3 ],
+				[ 0, 1, 2 ],
+				[ 1, 2, 3 ],
+				[ 2, 3, 0 ],
 			],
 		},
 		{
@@ -83,7 +98,10 @@ const constants = {
 			 */
 			type: 5,
 			outlets: [
-				[ 0, 1 ], [ 1, 2 ], [ 2, 3 ], [ 3, 0 ],
+				[ 0, 1 ],
+				[ 1, 2 ],
+				[ 2, 3 ],
+				[ 3, 0 ],
 			],
 		},
 	],
@@ -643,8 +661,6 @@ const animateElement = (row, column, ent) => {
 					const animatePromises = [];
 					const nextPromises = [];
 
-					updateElementsMap(element.type, row, column, element.direction, true);
-
 					await animateComponent('pipe-in', row, column, ent);
 
 					const spec = constants.elementsSpec.filter(e => e.type === element.type)[0];
@@ -670,6 +686,8 @@ const animateElement = (row, column, ent) => {
 					}
 
 					await Promise.all(nextPromises);
+
+					updateElementsMap(element.type, row, column, element.direction, true);
 					break;
 				}
 			}
