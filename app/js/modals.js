5123225ba40d4e9d8f60ea8c0d3fee5150e57068
Leonid Belikov <leonid@belikov.net>
2018-04-22 00:33:23 +0300

    modals upd

--- app/js/modals.js
+++ app/js/modals.js
@@ -211,7 +211,10 @@ const displayRulesModal = () => {
     rulesModal.className = 'modal medium';
     rulesModal.innerHTML = (`
     	<div>
-    		Using a variety of pipe pieces presented randomly in a queue, the player must construct a path from the start piece for the onrushing sewer slime, or "flooz", which begins flowing after a time delay from the start of the round. Pieces may not be rotated; they must be placed as presented in the queue. The player can replace a previously laid piece by clicking on it, as long as the flooz has not yet reached it; however, doing so causes a short time delay before the next piece can be laid. The flooz is required to pass through a given number of pipe pieces.
+    		<p>Using a variety of pipe pieces presented randomly in a queue, the player must construct a path from the start piece for the onrushing sewer slime, or "flooz", which begins flowing after a time delay from the start of the round.</p>
+    		<p>Pieces may not be rotated; they must be placed as presented in the queue.</p>
+    		<p>The player can replace a previously laid piece by clicking on it, as long as the flooz has not yet reached it; however, doing so causes a short time delay before the next piece can be laid.</p>
+    		<p>The flooz is required to pass through a given number of pipe pieces.</p>
     	</div>
     	<div class="submit-block">
     		<button id="return-to-menu">Go to menu</button>
